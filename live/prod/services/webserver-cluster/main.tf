8ad8224e676f39cbb320c5f46d20ef79a998ae5c
binyoma <innocentbinyoma@pm.me>
2025-02-26 07:08:29 +0100

    module'versions with remote rep

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -18,19 +18,34 @@ provider "aws" {
   }
 }
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
-  cluster_name = "webservers-stage"
+  source = "git::https://github.com/binyoma/terraform-modules//services/webserver-cluster?ref=v0.0.1"
+  cluster_name = "webservers-prod"
   db_remote_state_bucket = "terraform"
-  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
-  instance_type = "t2.micro"
-  max_size = 2
+  db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
+  instance_type = "m4.large"
+  max_size = 10
   min_size = 2
 }
-
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  autoscaling_group_name = module.webserver_cluster.asg_name
+  scheduled_action_name  = "scale-out-during-business-hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+}
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  autoscaling_group_name = module.webserver_cluster.asg_name
+  scheduled_action_name  = "scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+}
 terraform {
   backend "s3" {
     bucket = "terraform"
-    key = "stage/services/webserver-cluster/terraform.tfstate"
+    key = "prod/services/webserver-cluster/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform_locks"
     encrypt = true
--- live/stage/data-stores/mysql/main.tf
+++ live/stage/data-stores/mysql/main.tf
@@ -17,45 +17,31 @@ provider "aws" {
     elbv2          = "http://localhost:4566"
   }
 }
-module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
-  cluster_name = "webservers-prod"
-  db_remote_state_bucket = "terraform"
-  db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
-  instance_type = "m4.large"
-  max_size = 10
-  min_size = 2
+resource "aws_db_instance" "example" {
+  instance_class = "db.t2.micro"
+  identifier_prefix = "terraform"
+  engine = "mysql"
+  allocated_storage = 10
+  skip_final_snapshot = true
+  db_name = "example_database"
+
+  username = var.db_username
+  password = var.db_password
 }
-resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-  autoscaling_group_name = module.webserver_cluster.asg_name
-  scheduled_action_name  = "scale-out-during-business-hours"
-  min_size = 2
-  max_size = 10
-  desired_capacity = 10
-  recurrence = "0 9 * * *"
-}
-resource "aws_autoscaling_schedule" "scale_in_at_night" {
-  autoscaling_group_name = module.webserver_cluster.asg_name
-  scheduled_action_name  = "scale-in-at-night"
-  min_size = 2
-  max_size = 10
-  desired_capacity = 2
-  recurrence = "0 17 * * *"
-}
+
 terraform {
   backend "s3" {
     bucket = "terraform"
-    key = "prod/services/webserver-cluster/terraform.tfstate"
+    key = "stage/data-stores/mysql/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform_locks"
     encrypt = true
-
     endpoints                   = {
       s3 = "http://s3.localhost.localstack.cloud:4566"
     }
+    force_path_style            = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    force_path_style            = true
     access_key                  = "test"
     secret_key                  = "test"
   }
